3685d56d1e38fb5744e0c55de5f19320b803f373
Andrew Furrer <afurrer143@gmail.com>
2022-12-23 16:05:36 -0600

    Initial Test Page up

---
+++ frontend/src/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {BrowserRouter, Route, Link} from "react-router-dom"
+
+function LayOut(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default LayOut
